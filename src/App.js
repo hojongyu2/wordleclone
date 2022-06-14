7b44a46850244370cbe7aedaf7bc58b29c0d9722
Hojong Yu <hojongyu@Hojongs-MBP.home>
2022-06-13 23:35:29 -0400

    WordleList completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+const defaultGuessList = [
+  ["","","","",""],
+  ["","","","",""],
+  ["","","","",""],
+  ["","","","",""],
+  ["","","","",""],
+  ["","","","",""],
+  ]
+
 function App() {
+
+  const [wordleGuesslist, setWordleGuessList] = useState([...defaultGuessList])
+   
+  // const [letterGuess, setLetterGuess] = useState('')
+
+  // const [keyList, setKeyList] = useState ([
+  //   ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+  //   ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+  //   ["enter", "z", "x", "c", "v", "b", "n", "m", "backspace"]
+  //   ])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WordleGridHead/>
+        <WordleGrid
+          wordleGuesslist={wordleGuesslist}
+        />
+        <WordleKeyboard/>
       </header>
     </div>
   );
 }
 
+const WordleGridHead = () => {
+  return (
+    <h1 className='Wordle-Head'>
+      Wordle Clone
+    </h1>
+  )
+}
+const WordleGrid = (props) => {
+  // object destructure
+  // const  {wordleGuesslist} = props
+  return(
+    <div className="wordle-grid">
+      {props.wordleGuesslist.map((wordleGuess)=>{
+        return(
+          <GuessRow wordleGuess={wordleGuess}/>
+        )
+      })}
+    </div>
+  )
+}
+
+const GuessRow = (props) => {
+  return (
+    <div className="wordle-grid-row">
+      {props.wordleGuess.map((wordleLetter)=>{
+        return(
+          <GuessLetter wordleLetter={wordleLetter}/>
+        )
+      })}
+    </div>  
+  )
+}
+const GuessLetter = (props) => {
+  return (
+    <div className='wordle-grid-letter'>
+      {props.wordleLetter}
+    </div>
+  )
+}
+
+const WordleKeyboard = (props) =>{
+  
+  return (
+    <div>
+      WordleKeyboard
+    </div>
+  )
+}
+
+//componentize
 export default App;
